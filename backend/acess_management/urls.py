a82d34a05111bcddb931469623fc041c0022b07f
samundar <samundarsingh9525@gmail.com>
2024-04-19 05:12:41 +0530

    added default routes

--- backend/acess_management/urls.py
+++ backend/acess_management/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('members/', views.members, name='acess_management'),
+    path('', views.userData, name='acess_management'),
     path('users/', views.userData, name='acess_management'),
     path('fetch_data/<int:id>/', views.fetchUserData, name='acess_management'),
     path('updateUser/', views.updateUserData, name='acess_management'),
